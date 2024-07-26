e82931d3d584c982f8254beca8f5eb4f742b92d6
developersamari <samsa8784@gmail.com>
2024-07-27 01:21:17 +0330

    button and drag with inactiv

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import Image from "next/image";
-import eshterak from "./assent/imag/Wallet.svg"
- 
-export default function Home(){
-  return(
+
+export default function Home() {
+  return (
     <>
-    <div className="flex flex-row ">
-      <Image className="w-[18px] h-[16px]" src={eshterak} alt=""/>
-      <button>اقساط اشتراک</button>
-    </div>
-    <div>
-      <button>اقساط وام</button>
-    </div>
+   
     </>
   );
 }
